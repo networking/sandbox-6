622889e468baa7f30a80e79c43232afc2435ecc0
Yorkie Chadwick <y.chadwick@networking.ch>
2024-04-16 09:22:34 +0200

    Remove JS translations from git repository

--- public/js/translations/sr_RS.js
+++
@@ -1,5 +0,0 @@
-(function (t) {
-// sr_RS
-t.add("No results found", "Nema rezultata", "AutocompleteBundle", "sr_RS");
-t.add("No more results", "Nema vi\u0161e rezultata", "AutocompleteBundle", "sr_RS");
-})(Translator);
--- public/js/translations/sw.js
+++
@@ -1,6 +0,0 @@
-(function (t) {
-// sw
-t.add("label_previous", "Kabla", "KnpPaginatorBundle", "sw");
-t.add("label_next", "Ijayo", "KnpPaginatorBundle", "sw");
-t.add("filter_searchword", "Utaftaji...", "KnpPaginatorBundle", "sw");
-})(Translator);
